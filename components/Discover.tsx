3593ba91bbd0d126aaf0cbc533ac952db9ad078a
awais512 <raza.awais25@gmail.com>
2022-07-05 11:45:20 +0500

    Discover Components final

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -4,20 +4,25 @@ import React from "react";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
+  const router = useRouter();
+  const { topic } = router.query;
+
+  const activeTopicStyle =
+    "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#FF1997]";
+  const topicStyle =
+    "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
   return (
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
       <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
         Popular Topics
       </p>
       <div className="flex gap-3 flex-wrap">
-        {topics.map((topic, i) => (
-          <Link href={`/?topic=${topic.name}`} key={i}>
-            <div>
-              <span className="text-2xl font-bold xl:text-md">
-                {topic.icon}
-              </span>
+        {topics.map((top, i) => (
+          <Link href={`/?topic=${top.name}`} key={i}>
+            <div className={topic === top.name ? activeTopicStyle : topicStyle}>
+              <span className="text-2xl font-bold xl:text-md">{top.icon}</span>
               <span className="font-medium text-md hidden xl:block capitalize">
-                {topic.name}
+                {top.name}
               </span>
             </div>
           </Link>
