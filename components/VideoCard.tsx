e3a8dd883f51f156dc322f5aa2eb1ae6fd5c25e3
awais512 <raza.awais25@gmail.com>
2022-07-11 21:15:41 +0500

    VideoCard Component

---
+++ components/VideoCard.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Video } from "../types";
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import { BsPlay } from "react-icons/bs";
+
+interface IProps {
+  post: Video;
+}
+
+const VideoCard: NextPage<IProps> = ({ post }) => {
+  return (
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/" passHref>
+              <Image
+                src={post.postedBy.image}
+                width={62}
+                height={62}
+                className="rounded-full"
+                alt="Profile"
+                layout="responsive"
+              />
+            </Link>
+          </div>
+          <Link href="/">
+            <div className="flex gap-2 items-center">
+              <p className="flex items-center gap-2 md:text-md font-bold text-primary">
+                {post.postedBy.userName}{" "}
+                <GoVerified className="text-blue-400 text-md" />
+              </p>
+              <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                {post.postedBy.userName}
+              </p>
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="flex lg:ml-20 relative gap-4">
+        <div
+          onMouseEnter={() => {}}
+          onMouseLeave={() => {}}
+          className="rounded-3xl"
+        >
+          <Link href="/">
+            <video
+              src={post.video.asset.url}
+              loop
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+            ></video>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
