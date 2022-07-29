289d4ed547c8a5b1c1e2b7bd3b7f3c6f4fc01116
awais512 <raza.awais25@gmail.com>
2022-07-29 14:42:18 +0500

    No Result component

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+
+import useAuthStore from "../store/authStore";
+import NoResult from "./NoResult";
+import { IUser } from "../types";
 
 const Comments = () => {
-  return <div>Comments</div>;
+  const comments = [];
+  return (
+    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
+      <div className="overflow-scroll lg:h-[470px]">
+        {comments.length ? (
+          <div>Videos</div>
+        ) : (
+          <div>
+            <NoResult text="No Comments Yet" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
