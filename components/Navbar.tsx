1077bcddc1412c95b5fa99a9866a28a6c7da11a2
awais512 <raza.awais25@gmail.com>
2022-07-30 22:34:42 +0500

    Ready for deployment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,46 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
-import Logo from "../utils/tiktik-logo.png";
-import { createOrGetUser } from "../utils";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+
 import useAuthStore from "../store/authStore";
+import { IUser } from "../types";
+import { createOrGetUser } from "../utils";
+import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
-  const { userProfile, addUser, removeUser } = useAuthStore();
+  const [user, setUser] = useState<IUser | null>();
   const [searchValue, setSearchValue] = useState("");
-
   const router = useRouter();
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
+  useEffect(() => {
+    setUser(userProfile);
+  }, [userProfile]);
 
   const handleSearch = (e: { preventDefault: () => void }) => {
     e.preventDefault();
+
     if (searchValue) {
       router.push(`/search/${searchValue}`);
     }
   };
 
   return (
-    <div className="w-full flex items-center justify-between border-b-2 border-gray-200 py-2 px-4">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className="w-[100px] md:w-[130px] cursor-pointer">
-          <Image src={Logo} alt="Logo" layout="responsive" />
+        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="logo"
+            layout="responsive"
+          />
         </div>
       </Link>
+
       <div className="relative hidden md:block">
         <form
           onSubmit={handleSearch}
           className="absolute md:static top-10 -left-20 bg-white"
         >
           <input
-            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0"
-            type="text"
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Search Accounts and Videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
+            placeholder="Search accounts and videos"
           />
           <button
             onClick={handleSearch}
@@ -51,28 +63,30 @@ const Navbar = () => {
         </form>
       </div>
       <div>
-        {userProfile ? (
-          <div className="cursor-pointer flex gap-5 md:gap-10">
+        {user ? (
+          <div className="flex gap-5 md:gap-10">
             <Link href="/upload">
-              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2 bg-pink-700 text-white hover:bg-pink-400 rounded-md p-1 transition-all ease-in-out">
-                <IoMdAdd className="text-xl" />
-                {` `} <span className="hidden md:block">Upload</span>
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" />{" "}
+                <span className="hidden md:block">Upload </span>
               </button>
             </Link>
-            {userProfile.image && (
-              <Link href="/">
-                <Image
-                  src={userProfile.image}
-                  width={40}
-                  height={40}
-                  className="rounded-full cursor-pointer"
-                  alt="Profile"
-                />
+            {user.image && (
+              <Link href={`/profile/${user._id}`}>
+                <div>
+                  <Image
+                    className="rounded-full cursor-pointer"
+                    src={user.image}
+                    alt="user"
+                    width={40}
+                    height={40}
+                  />
+                </div>
               </Link>
             )}
             <button
-              className="px-2"
               type="button"
+              className=" border-2 p-2 rounded-full cursor-pointer outline-none shadow-md"
               onClick={() => {
                 googleLogout();
                 removeUser();
@@ -83,10 +97,8 @@ const Navbar = () => {
           </div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => {
-              createOrGetUser(response, addUser);
-            }}
-            onError={() => console.log("Error")}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log("Login Failed")}
           />
         )}
       </div>
